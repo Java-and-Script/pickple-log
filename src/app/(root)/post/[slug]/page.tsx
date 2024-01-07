1fcadc12f0cf488f199de0c980f491786f9eb825
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-01-07 22:35:11 +0900

    chore: tailwindcss/typography 적용
    * 다크모드 적용

--- src/app/(root)/post/[slug]/page.tsx
+++ src/app/(root)/post/[slug]/page.tsx
@@ -4,8 +4,17 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 export default function Page({ params }: { params: { slug: string } }) {
   const mdxSource = getPostBySlug(params.slug).content;
   return (
-    <div>
-      <MDXRemote source={mdxSource} />
+    <div className="prose dark:text-white">
+      <MDXRemote
+        source={mdxSource}
+        options={{
+          mdxOptions: {
+            useDynamicImport: true,
+            remarkPlugins: [],
+            rehypePlugins: [],
+          },
+        }}
+      />
     </div>
   );
 }
