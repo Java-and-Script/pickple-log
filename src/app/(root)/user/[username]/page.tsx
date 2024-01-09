9ed56dc40eddeeb2f3540e41cd1e8c3088797089
dlwl98 <ff981113@hanyang.ac.kr>
2024-01-09 21:04:58 +0900

    feat: user 페이지 static 페이지 생성

--- src/app/(root)/user/[username]/page.tsx
+++ src/app/(root)/user/[username]/page.tsx
@@ -1,5 +1,10 @@
-import { getPostDatasByUsername } from '@/app/utils';
+import { getPostDatasByUsername, getUsers } from '@/app/utils';
 
 export default function Page({ params }: { params: { username: string } }) {
   return <div>{JSON.stringify(getPostDatasByUsername(params.username))}</div>;
+}
+
+export function generateStaticParams() {
+  const users = getUsers();
+  return users.map((user) => ({ username: user.name }));
 }
