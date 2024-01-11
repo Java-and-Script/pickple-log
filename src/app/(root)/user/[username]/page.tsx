fb344d42420835e4ab9e9d0d7fc20960252dfeeb
1g2g <wonji126326@gmail.com>
2024-01-11 16:32:32 +0900

    feat: 유저 페이지에 유저 컴포넌트 추가

--- src/app/(root)/user/[username]/page.tsx
+++ src/app/(root)/user/[username]/page.tsx
@@ -1,10 +1,27 @@
+import BackButton from './_components/BackButton';
+import { MemberItem } from '@/app/_components/MemberItem';
 import { PostItemList } from '@/app/_components/PostItemList';
-import { getPostDatasByUsername, getUsers } from '@/app/utils';
+import { getPostDatasByUsername, getUser, getUsers } from '@/app/utils';
+import BackArrow from '@/assets/backArrow.svg';
+import Image from 'next/image';
 
-export default function Page({ params }: { params: { username: string } }) {
-  const postDatas = getPostDatasByUsername(params.username);
+export default function Page({
+  params: { username },
+}: {
+  params: { username: string };
+}) {
+  const postDatas = getPostDatasByUsername(username);
+  const userData = getUser(username);
 
-  return <PostItemList postDatas={postDatas} />;
+  return (
+    <>
+      <BackButton>다른 팀원 보기</BackButton>
+      <div className="flex flex-col gap-4">
+        <MemberItem key={username} {...userData} />
+        <PostItemList postDatas={postDatas} />
+      </div>
+    </>
+  );
 }
 
 export function generateStaticParams() {
